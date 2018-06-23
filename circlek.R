c41d0c20dc8326983d9c81e4ae6d45a2dca1430a
Gu <mm32908@mm32908m1.local>
2018-06-23 09:54:03 +0800

    first commt

---
+++ circlek.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+# Read the data----------------------------------------------------------------
+prom <- read_csv("data/promotions1.csv")
+trans <- read_csv("data/transactions.csv")
+
