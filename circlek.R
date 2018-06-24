2615460b71efd26900b08518435647fe509ca8ea
Cecilia Lee <ceciliahslee@gmail.com>
2018-06-24 12:51:28 +0800

    Delete circlek

--- circlek.R
+++
@@ -1,6 +0,0 @@
-library(tidyverse)
-
-# Read the data----------------------------------------------------------------
-prom <- read_csv("data/promotions1.csv")
-trans <- read_csv("data/transactions.csv")
-
